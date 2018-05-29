817d5172cd4b8f310b8df6a0c47befda8b2f9296
mohammedajao <ajaomohammed@gmail.com>
2018-05-28 20:50:44 -0400

    Initial commit

---
+++ data.py
@@ -0,0 +1,11 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title': 'Article One',
+      'body': 'Oof',
+      'author': 'Moe',
+      'create date':'05-26-2018'
+    }
+  ]
+  return articles
